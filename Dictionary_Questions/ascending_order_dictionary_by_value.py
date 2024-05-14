2835a111d77df8ca62178ea1af8da49f6947423c
Keshav Sharma <keshavpradeepsharma@gmail.com>
2024-05-14 16:55:41 +0530

    Update ascending_order_dictionary_by_value.py

--- Dictionary_Questions/ascending_order_dictionary_by_value.py
+++ Dictionary_Questions/ascending_order_dictionary_by_value.py
@@ -6,3 +6,5 @@ for i in range(num):
     d[key]=value
 sorted_d=sorted(d.items(),key=lambda x:x[1])
 print(sorted_d)
+# d2=d1.copy is a command used to make a refrence where we have taken a snapshot of the situation and changes in d1 will not affect d2
+# whereas d2=d1, in this changes in d1 will affect d2 
